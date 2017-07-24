d41a24972edbe7a6795364ee8d8fe19358d937cc
darshan4694 <darshan.shah4694@gmail.com>
2017-07-24 15:30:32 -0500

    added app.render

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ app.get('/',(req, res) => {
     res.render('index');
 });
 
-app.get('/first',(req, res) => {
-    res.send("This is first page!");
+app.get('/cards',(req, res) => {
+    res.render('card',{prompt: "Name 3 characteristics of leadership?", hint: "It includes Motivation and leading."});
+    // res.send("This is first page!");
 });
 
 app.listen(3000, () => {
