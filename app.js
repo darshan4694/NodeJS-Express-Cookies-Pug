ade542ca71526e3a668da980871a668eb623861c
darshan4694 <darshan.shah4694@gmail.com>
2017-07-24 16:47:16 -0500

    Handling POST Request also used body-parser module

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 var app = express();
 
-app.set('view engine', 'pug');
+app.use(bodyParser.urlencoded({ extended: false }));
 
-const colors = [
-  'red',
-  'orange',
-  'yellow',
-  'green',
-  'blue',
-  'purple'
-];
+app.set('view engine', 'pug');
 
 app.get('/',(req, res) => {
     res.render('index');
@@ -21,6 +15,15 @@ app.get('/cards',(req, res) => {
     res.render('card',{prompt: "Name 3 characteristics of leadership?", 
     hint: "It includes Motivation and leading.", colors});
     // res.send("This is first page!");
+});
+
+app.get('/hello', (req, res) => {
+    res.render('hello');
+});
+
+app.post('/hello', (req, res) => {
+    //console.dir(req.body);
+    res.render('hello', {name: req.body.username});
 });
 
 app.listen(3000, () => {
