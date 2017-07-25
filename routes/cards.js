00a1e540f810db7e5a6a6ac6c4bfe8734d31575e
darshan4694 <darshan.shah4694@gmail.com>
2017-07-25 00:25:02 -0500

    Handled Query string and ('a' in card.pug to implement links)

--- routes/cards.js
+++ routes/cards.js
@@ -4,9 +4,23 @@ const router = express.Router();
 const { data } = require('../data/flashCardsData');
 const { cards } = data;
 
-router.get('/:id',(req, res) => {
-    res.render('card',{prompt: cards[req.params.id].question, 
-    hint: cards[req.params.id].hint});
+router.get('/:id', (req, res) => {
+
+    const { side } = req.query;
+    const { id } = req.params;
+    const text = cards[id][side];
+    const templateData = { text };
+    const {hint} = cards[id];
+    const link = '/cards/'+id;
+    if (side === 'question') {
+        //const { hint } = cards[id];
+        templateData.link = link + '?side=answer';
+        templateData.hint = hint;
+    } else {
+        templateData.link = link + '?side=question';
+    }
+    //console.log(side);
+    res.render('card', templateData);
 });
 
 module.exports = router;
